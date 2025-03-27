17366054affb1c5fea2ba5bd4daecc779dd5ad76
perhoff <per.hoff@soprasteria.com>
2025-03-27 13:26:53 +0100

    Update tasts.md file

---
+++ CodeAcademy/Controllers/CarController.cs
@@ -0,0 +1,23 @@
+﻿using CodeAcademy.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeAcademy.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class CarController
+    {
+        [HttpGet(Name = "GetCar")]
+        public CarDto GetCarById()
+        {
+            return new CarDto
+            {
+                Brand = CarBrand.Audi,
+                Color = "Red",
+                Id = 1,
+                Name = "Q7",
+                Price = 10000
+            };
+        }
+    }
+}
